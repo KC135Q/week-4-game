125ecce9ca320dd79b4a10e82b136e852f80cbe3
Daniel Kaltenbaugh <d.a.kaltenbaugh@gmail.com>
2016-07-18 09:13:15 -0400

    Player and Opponent selection logic complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,9 @@ $(document).ready(function() {
 	drivers[2] = new Driver ('Senna', 'senna.jpg', 140, 30, 35);
 	drivers[3] = new Driver ('Top Dog', 'top-dog.jpg', 150, 35, 40);
 
+	var player = -1;
+	var opponent = -1;
+
 	function Driver(name, image, health, attack, counter) {
 		this.name = name;
 		this.image = image;
@@ -13,26 +16,64 @@ $(document).ready(function() {
 		this.counter = counter;
 	}
 
-	function addDrivers(drivers) {
+	function showDriverPool() {
+		$('.drivers').empty();
 		for (ctr = 0; ctr < drivers.length; ctr++) {
-			// console.log(drivers[ctr].image);
-			// var $newDriver = $('<div class="driver col-sm-3"><span class="name">'+ drivers[ctr].name +'</span><img src="assets/images/'+ drivers[ctr].image +'"><span class="points">'+ drivers[ctr].health +'</span></div>');
-			var $newDriver = $('<div>')
-				.addClass('driver col-sm-3')
-				.attr('driver-id', ctr)
-				.html('<span class="name">'+ drivers[ctr].name +'</span><img src="assets/images/'+ drivers[ctr].image +'"><span class="points">'+ drivers[ctr].health +'</span>');
-				// $newDriver.on('click', selectDriver(ctr));
-			$('.drivers').append($newDriver);
-		}		
+			// Check to see if they have been selected to play first
+			// Will also need to check if they have been eliminated
+			if (ctr != player && ctr != opponent) {
+				var $newDriver = $('<div>')
+					.addClass('driver col-sm-3')
+					.attr('driver-id', ctr)
+					.html('<span class="name">'+ drivers[ctr].name +'</span><img src="assets/images/'+ drivers[ctr].image +'"><span class="points">'+ drivers[ctr].health +'</span>');
+					// $newDriver.on('click', selectDriver(ctr));
+				$('.drivers').append($newDriver);				
+			}
+		}
+		$('.driver').on('click', function() {
+			selectDriver(this.getAttribute('driver-id'));
+		});
+	}
+
+	function showPlayer(index) {
+		var $newPlayer = $('<div>')
+			.addClass(' player col-sm-offset-1 col-sm-3 ')
+			.html('<span class="name">'+ drivers[index].name +'</span><img src="assets/images/'+ drivers[index].image +'"><span class="points">'+ drivers[index].health +'</span>');
+		$('#player').html($newPlayer);
+		$('#player-header').html('Player');
+		$('#versus').html('<img src="assets/images/vs.png">');
+	}
+	function showOpponent(index) {
+		var $newOpponent = $('<div>')
+			.addClass('opponent col-sm-offset-1 col-sm-3 ')
+			.html('<span class="name">'+ drivers[index].name +'</span><img src="assets/images/'+ drivers[index].image +'"><span class="points">'+ drivers[index].health +'</span>');
+		$('#opponent').html($newOpponent);
+		$('#opponent-header').html('Opponent');
+	}
+	function refreshDisplay () {
+		console.log("Player: " + player + ", Opponent: " + opponent);
+		showDriverPool();
+		if (player != -1) {
+			showPlayer(player);
+		}
+		if (opponent != -1) {
+			showOpponent(opponent);
+		}
 	}
 
 	function selectDriver(index) {
 		console.log("Index " + index);
+		if (player == -1 && opponent === -1 ) {
+			// Nothing selected so this is the player
+			player = index;
+		} else if (opponent === -1) {
+			// player already picked, so pick the opponent
+			opponent = index;
+		}
+		refreshDisplay();
 	}
 
-	addDrivers(drivers);
-	$('.driver').on('click', function() {
-		selectDriver(this.getAttribute('driver-id'));
-	});
+	showDriverPool();
+
 	// console.log(drivers);
 });
